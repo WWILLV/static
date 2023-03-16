2461821a0d73f205a02ce687cbbccd4bc3c1e17e
WILL_V <767436053@qq.com>
2023-03-16 20:46:20 +0900

    Add files via upload

--- js/getcatche.js
+++ js/getcatche.js
@@ -1,1 +1,8 @@
-alert(1);
+window.onload=function(){
+    var h="<img id='imgsrc' src='' style='object-fit: cover;'></img>"
+    fetch('https://dynamic.willv.workers.dev/').then((response)=>{
+        document.getElementById("imgsrc").setAttribute("src",response.text());
+       });
+    document.getElementById("catche").innerHTML=h;
+    document.getElementById("last").innerText="2023-03-15";
+}
